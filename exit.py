c8ea6770d055d891c4c95827088e962f9b10f0cd
Saeideh-Moghaddam <108235776+saeideh-moghaddam@users.noreply.github.com>
2022-06-29 22:20:03 +0430

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,15 @@
+import math
+a = 0
+while  True :
+
+    num = input("Please type your number for end enter exit : ")
+
+    if num == "exit" :
+      break
+    
+    else :
+        
+         
+         a = int(num) + a 
+
+print (a)    
